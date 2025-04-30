acb4efdfb1e8c7e79ed87b01c6cdf32b240264ae
ABCchocolate <qrkdbtls@gmail.com>
2025-04-30 12:31:53 +0900

    2차원 배열

---
+++ Back_workspace/java_workspace/Day3/DataType.java
@@ -0,0 +1,13 @@
+class  DataType
+{	
+	public static void main(String[] args) 
+	{
+		short s = 9;
+		byte b = 7;
+		long  k = 10;
+		char c= 'k';
+		int sum = c + b;
+		System.out.print(sum);
+		
+	}
+}
