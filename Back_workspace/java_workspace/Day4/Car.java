626d2bd636e22c21adb42906b1b9137abfd29e60
ABCchocolate <qrkdbtls@gmail.com>
2025-05-07 17:56:32 +0900

    5월 7일 수업 1. 총알 게임 작성 2. 이퀄라이저 작성 3. SQL에서 SEQUENCE 쿼리 이론 배움

---
+++ Back_workspace/java_workspace/Day4/Car.java
@@ -0,0 +1,10 @@
+class Car 
+{
+	int price = 3000;
+	String color = "red";
+	
+	public void car(){
+		System.out.println("가격은 " + price + " 입니다.");
+		System.out.println("색상은 " + color + " 입니다.");
+	}
+}
