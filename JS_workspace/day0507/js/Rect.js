0cf68a48b07242ca58e8d65ad4d5319b24abfa9c
ABCchocolate <qrkdbtls@gmail.com>
2025-05-08 08:38:11 +0900

    이퀄라이저 기능 추가 - 막대바의  Y 축 반전 효과 추가 - 막대바 움직임 속도 조절 기능 추가

--- JS_workspace/day0507/js/Rect.js
+++ JS_workspace/day0507/js/Rect.js
@@ -17,27 +17,22 @@ class Rect {
 
         // 화면에 부착
         this.container.appendChild(this.div);
-        
-        // 애니메이션 시작
+
+    
         this.move();
     }
 
-    // 막대의 높이를 랜덤으로 변화시키는 함수
     move() {
-        // 높이를 랜덤으로 바꿔주는 애니메이션
         setInterval(() => {
-            // 목표 높이를 랜덤으로 설정 (이퀄라이저처럼 높이가 바뀐다)
-            let randomHeight = Math.random() * 400 + 50; // 최소 50px, 최대 450px로 설정
+            let randomHeight = Math.random() * 400 + 50; 
 
-            // 막대의 높이를 점진적으로 변경
             let currentHeight = parseFloat(this.div.style.height);
-            let speed = 0.1; // 변화 속도 (점진적으로 높이를 증가)
+            let speed = 1.0;
 
-            // 목표 높이로 점진적으로 이동
             if (Math.abs(randomHeight - currentHeight) > 1) {
                 currentHeight += (randomHeight - currentHeight) * speed;
                 this.div.style.height = currentHeight + "px";
             }
-        }, 100); // 100ms마다 높이 변화
+        }, 100);
     }
 }
