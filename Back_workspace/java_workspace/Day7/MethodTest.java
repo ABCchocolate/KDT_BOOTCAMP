580c5fb7c44e4bc8fe5db7d88dce9a6032237a0e
ABCchocolate <qrkdbtls@gmail.com>
2025-05-12 17:50:28 +0900

    js수업의 일환으로 API연동에 대해 배움

---
+++ Back_workspace/java_workspace/Day7/MethodTest.java
@@ -0,0 +1,13 @@
+ //Method의 개념: 클래스에 소속된 함수를 가르켜 Method라고 한다.
+class MethodTest 
+{
+	//반환형이 없는 Mthod
+	public void getName(String a){
+		
+	}
+		
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
