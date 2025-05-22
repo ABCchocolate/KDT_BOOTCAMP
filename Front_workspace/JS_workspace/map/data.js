c85766b159775983ed1fef3617c3a8e97b530003
ABCchocolate <qrkdbtls@gmail.com>
2025-05-22 09:05:31 +0900

    5월 21일자 수업 정리

---
+++ Front_workspace/JS_workspace/map/data.js
@@ -0,0 +1,29 @@
+const places = [
+{
+    id: 1,
+    name: "별다방",
+    category: "카페",
+    x: 2,
+    y: 1,
+    description: "커피 맛집. 콘센트 많음.",
+    image: "https://via.placeholder.com/300x150?text=별다방"
+},
+{
+    id: 2,
+    name: "가츠연구소",
+    category: "음식점",
+    x: 4,
+    y: 3,
+    description: "수제 돈가스가 맛있는 곳",
+    image: "https://via.placeholder.com/300x150?text=가츠연구소"
+  },
+  {
+    id: 3,
+    name: "센트럴파크",
+    category: "공원",
+    x: 1,
+    y: 5,
+    description: "산책하기 좋은 공원입니다.",
+    image: "https://via.placeholder.com/300x150?text=센트럴파크"
+  },
+];
